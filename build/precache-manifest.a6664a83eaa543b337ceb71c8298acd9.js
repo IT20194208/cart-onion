62a58333a0438a6265fd1818809ec6f09836d0c2
Ranasinghe R.P.C.P <73824269+IT20194208@users.noreply.github.com>
2024-05-23 12:42:47 +0530

    de

---
+++ build/precache-manifest.a6664a83eaa543b337ceb71c8298acd9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5621a727b2684648a1517ec893d7124d",
+    "url": "/https//it20194208.github.io/cart-onion/index.html"
+  },
+  {
+    "revision": "e7d1fa539f5377cfd134",
+    "url": "/https//it20194208.github.io/cart-onion/static/css/2.e7c4c567.chunk.css"
+  },
+  {
+    "revision": "561e6000e0b092177d20",
+    "url": "/https//it20194208.github.io/cart-onion/static/css/main.3097511c.chunk.css"
+  },
+  {
+    "revision": "e7d1fa539f5377cfd134",
+    "url": "/https//it20194208.github.io/cart-onion/static/js/2.b88cf649.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/https//it20194208.github.io/cart-onion/static/js/2.b88cf649.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "561e6000e0b092177d20",
+    "url": "/https//it20194208.github.io/cart-onion/static/js/main.200a750a.chunk.js"
+  },
+  {
+    "revision": "51c324a87b3bbe3e66ab",
+    "url": "/https//it20194208.github.io/cart-onion/static/js/runtime-main.f865e5c8.js"
+  }
+]);
